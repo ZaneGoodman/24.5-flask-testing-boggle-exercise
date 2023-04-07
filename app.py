7951ef56e07bb242598ef11a942ef259672f7dc7
Zane_Goodman <zgoodman1776@gmail.com>
2023-04-07 11:15:20 -0400

    continued

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, render_template, session, redirect, request,jsonify
+from flask import Flask, render_template, session, redirect, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -17,15 +17,13 @@ def make_and_show_board():
     session["board"] = board
     return render_template('show_board.html')
 
+
 def is_valid_word(word):
-    return Boggle.check_valid_word(boggle_game,session["board"], word)
+    return Boggle.check_valid_word(boggle_game, session["board"], word)
 
 
-@app.route('/check-word', methods=["POST"])
+@app.route('/check-word', methods=["POST", "GET"])
 def check_if_valid_word():
-    user_guess =  request.get_json(force=True)['data']['guess']
+    user_guess = request.get_json(force=True)['data']['guess']
     result = is_valid_word(user_guess)
-    print(result)
-    return redirect("/")
-
-
+    return jsonify(result)
--- static/app.js
+++ static/app.js
@@ -1,15 +1,21 @@
-function test() {
-  console.log("working");
-}
-
-
 async function checkIfValidWord(evt) {
-  evt.preventDefault()
-  console.log('working')
-  res = await axios.post("/check-word" , {
+  evt.preventDefault();
+  console.log("working");
+
+  res = await axios.get("/check-word", {
     data: {
-      guess: $("#guess").val()
-    }
+      guess: $("#guess").val(),
+    },
   });
+  if (res.data === "not-word") {
+    showMessage("not a word");
+  }
 }
-$("#guessForm").on("submit", checkIfValidWord)
+
+$("#guessForm").on("submit", checkIfValidWord);
+
+function showMessage(msg) {
+  $(".msg").text(msg);
+}
+
+// why can I not affect the dom with messages?
